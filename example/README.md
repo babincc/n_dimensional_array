c5d94bccc8ed651f31ce86b817314ebd54605c9b
Christian Babin <cssbabin@gmail.com>
2025-02-20 00:38:56 -0500

    Moved old code to this repo

---
+++ example/README.md
@@ -0,0 +1,3 @@
+# example
+
+Demonstrates how to use the n_dimensional_array package.
---
+++ lib/n_dimensional_array.dart
@@ -0,0 +1,6 @@
+// @author Christian Babin
+// @version 1.0.4
+// https://github.com/babincc/n_dimensional_array/blob/master/lib/n_dimensional_array.dart
+
+export 'package:n_dimensional_array/domain/models/nd_array.dart';
+export 'package:n_dimensional_array/utils/list_tools.dart';
